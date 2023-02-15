d90dc8146d0c0383e117362dfb1d5fdbc09b9fe4
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-02-15 11:42:19 -0500

    created api for todo app w get post and patch endpoints

---
+++ functions/src/utils.js
@@ -0,0 +1,14 @@
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import service_account from "./service_account.json" assert { type: "json" };
+
+export function getFirestoreInstance() {
+    //check if apps has already been initialized
+    const isInitialized = getApps().length > 0;
+    if (!isInitialized) { //not initialized, connect to firebase
+        initializeApp({
+            credential: cert(service_account), 
+        });
+    }
+    return getFirestore();
+}
